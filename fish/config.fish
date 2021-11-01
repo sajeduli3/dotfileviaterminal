771b7e93c1f35f2a36737bc9a0828764d1289f79
Sajedull <sajedul6@gmail.com>
2021-11-01 14:53:46 +0600

    yml and fish update

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-wal -i ~/Pictures/wallpaper/
-cat ~/.cache/wal/sequences &  
-~/.config/polybar/shapes/scripts/pywal.sh ~/Pictures/wallpaper
 
-
-clear
+colorscript -r
 
 end
